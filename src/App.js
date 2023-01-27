21fa11f020a18d7f6a15c8eb084f3c7fae2302a4
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2023-01-27 20:50:04 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import {Routes, Route} from 'react-router-dom'
+import Nav from './components/Nav';
+import Home from './pages/Home';
 import './App.css';
+
+/*npm i react-router-dom*/
+
+function About(){
+  return(
+    <div>
+      <h2>lala</h2>
+    </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+        <Route path="/" element={<Nav/>}>
+        <Route index element={<Home/>}/>
+        <Route path='about' element={<About/>}/>
+        </Route>
+    </Routes>
+    </>
   );
 }
 
