7b1adc6660684e15175e282204c8437e18de9db3
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2023-01-28 03:54:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,12 @@ import './App.css';
 
 /*npm i react-router-dom*/
 
-function About(){
-  return(
-    <div>
-      <h2>lala</h2>
-    </div>
-  )
-}
-
-
 function App() {
   return (
     <>
     <Routes>
         <Route path="/" element={<Nav/>}>
         <Route index element={<Home/>}/>
-        <Route path='about' element={<About/>}/>
         </Route>
     </Routes>
     </>
